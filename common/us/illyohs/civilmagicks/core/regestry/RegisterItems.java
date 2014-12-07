b5a1f1ffcc7a576e2f32eab143043903c4c82619
Anthony Anderson <LordIllyohs@gmail.com>
2014-12-07 03:26:06 -0500

    Illy can't spell
    Signed-off-by: Anthony Anderson <LordIllyohs@gmail.com>

--- common/us/illyohs/civilmagicks/core/regestry/RegisterItems.java
+++ common/us/illyohs/civilmagicks/core/regestry/RegisterItems.java
@@ -23,7 +23,7 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package us.illyohs.civilmagicks.core.regesty;
+package us.illyohs.civilmagicks.core.regestry;
 
 import net.minecraft.init.Items;
 
