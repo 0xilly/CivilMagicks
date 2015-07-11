baef2778f41af0749e3aec7482b0f60e68d74947
Anthony Anderson <LordIllyohs@gmail.com>
2015-07-10 21:33:26 -0400

    Really lazy today not much done for writs
    Signed-off-by: Anthony Anderson <LordIllyohs@gmail.com>

--- src/main/java/us/illyohs/civilmagicks/common/item/writ/ItemWrit.java
+++ src/main/java/us/illyohs/civilmagicks/common/item/writ/ItemWrit.java
@@ -1,27 +1,16 @@
 package us.illyohs.civilmagicks.common.item.writ;
 
-import us.illyohs.civilmagicks.api.CivilMagicksAPI;
-import us.illyohs.civilmagicks.api.writ.WritBase;
 import us.illyohs.civilmagicks.client.core.creativetab.CivilTab;
-import us.illyohs.civilmagicks.common.writ.TestWrit;
+import us.illyohs.civilmagicks.common.core.lib.LibInfo;
 import net.minecraft.item.Item;
 
 public class ItemWrit extends Item {
     
-    public static WritBase testWrit;
-    
-    public ItemWrit() {
-        testWrit = new TestWrit();;
-        
-        regWrit();
-    }
-
-    /**
-     * 
-     */
-    private void regWrit() {
-        CivilMagicksAPI.registerWrit(testWrit);
-        
-    }
+	public ItemWrit() {
+		setCreativeTab(CivilTab.BLOCKS);
+		setUnlocalizedName(LibInfo.MOD_NAME + ":writ");
+	}
+	
+	
 
 }
