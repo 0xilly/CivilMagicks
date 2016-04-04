10fba2963913aa241485696dfd9bc3f15fcc58d9
Anthony Anderson <lordillyohs@gmail.com>
2016-04-04 11:25:56 -0400

    Moving Workspacees
    Signed-off-by: Anthony Anderson <lordillyohs@gmail.com>

--- src/main/java/us/illyohs/civilmagiks/common/item/ItemWellSpawner.java
+++ src/main/java/us/illyohs/civilmagiks/common/item/ItemWellSpawner.java
@@ -30,8 +30,8 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import us.illyohs.libilly.item.ItemBase;
 
