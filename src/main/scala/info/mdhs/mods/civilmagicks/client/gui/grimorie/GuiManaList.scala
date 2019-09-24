b85f0d5b6d9002a7d9defeaf5608df294e424146
Anthony Anderson <anderson@mdhs.info>
2019-09-24 00:48:53 -0400

    Starting work on the ingame book
    Signed-off-by: Anthony Anderson <anderson@mdhs.info>

---
+++ src/main/scala/info/mdhs/mods/civilmagicks/client/gui/grimorie/GuiManaList.scala
@@ -0,0 +1,6 @@
+package info.mdhs.mods.civilmagicks.client.gui.grimorie
+
+import net.minecraft.client.gui.screen.Screen
+import net.minecraft.util.text.ITextComponent
+
+class GuiManaList(title: ITextComponent) extends Screen(title) {}
